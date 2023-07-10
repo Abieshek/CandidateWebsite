048ed540f2afb520db3b72f5a47ad1a1f4bce2d7
Abieshek Subramaniam <abieshek.subramaniam@gmail.com>
2023-07-09 22:12:33 -0400

    Fixing website refresh issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-export class AppComponent implements OnInit {
-  ngOnInit() {
-    window.location.reload()
-  }
-  
+export class AppComponent {
   title = 'candidateWebsite';
 }
-
-
-
-
-
-
